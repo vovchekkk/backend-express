b53e8d9cc2ca486031f079cbb9a722e761e85311
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-03-24 17:28:05 +0500

    8.1 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send({
