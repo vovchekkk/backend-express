44bfa90a3e10a3b126f093987c28d54109e997bc
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-03-24 17:07:51 +0500

    3 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items: [
+      {
+        "id": 1,
+        "name": "Вова"
+      },
+      {
+        "id": 2,
+        "name": "Никита"
+      }
+    ]
+  });
 });
 
 module.exports = router;
